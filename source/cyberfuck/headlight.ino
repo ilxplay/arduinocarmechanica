63321f04fa6b796ab620969e22e83f24fa05d98a
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-03-27 10:39:33 +0100

    rage

--- source/cyberfuck/headlight.ino
+++ source/cyberfuck/headlight.ino
@@ -1,7 +1,7 @@
 int ledPin = 11;  // LED connected to digital pin 10
 
 void headlightloop() {
-  int value = (1023 - analogRead(A5)) / 4;
+  int value = (1023 - analogRead(A1)) / 4;
   if (value < 50) {value = 0;}
   value = abs(value);
   analogWrite(ledPin, value);
